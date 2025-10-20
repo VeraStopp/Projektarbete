f45c480c4b645b71082ff2eb3f74486219691221
Cym3ra <carina.tmkarlsson@gmail.com>
2025-10-20 14:09:40 +0200

    en funktion för att hämta intäkt per kategori

--- src/metrics.py
+++ src/metrics.py
@@ -2,3 +2,8 @@ def calculating_revenue_city(df):
     result = df.groupby("city")["revenue"].sum().reset_index()
     result = result.sort_values(by="revenue", ascending=False).reset_index(drop=True)
     return result
+
+def calculate_category_rev(df):
+    summary = df.groupby("category")["revenue"].sum().reset_index()
+    summary = summary.sort_values(by="revenue", ascending=False)
+    return summary
