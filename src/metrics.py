483249a7f72ca4d12b5284286a344d7942df5419
Vera Stopp <vc.stopp@gmail.com>
2025-10-22 12:19:16 +0200

    Städade upp lite

--- src/metrics.py
+++ src/metrics.py
@@ -1,12 +1,12 @@
 def calculating_revenue_city(df):
-    result = df.groupby("city")["revenue"].sum().reset_index()
-    result = result.sort_values(by="revenue", ascending=False).reset_index(drop=True)
-    return result
+    result_city = df.groupby("city")["revenue"].sum().reset_index()
+    result_city = result_city.sort_values(by="revenue", ascending=False).reset_index(drop=True)
+    return result_city
 
 def calculate_category_rev(df):
-    summary = df.groupby("category")["revenue"].sum().reset_index()
-    summary = summary.sort_values(by="revenue", ascending=False)
-    return summary
+    result_cat = df.groupby("category")["revenue"].sum().reset_index()
+    result_cat = result_cat.sort_values(by="revenue", ascending=False)
+    return result_cat
 
 def calculate_average_order_value(df):   
     total_revenue = df['revenue'].sum()             #get total revenue from CSV 
@@ -16,6 +16,4 @@ def calculate_average_order_value(df):
     average_order_value = total_revenue / total_unique_orders
 
     return average_order_value, total_revenue
-
-
-
+    
